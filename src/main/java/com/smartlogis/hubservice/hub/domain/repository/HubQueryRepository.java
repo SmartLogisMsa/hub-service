c43226817b995d717d120499315fb9d9f5b361f8
nineeko <gkxm1013@naver.com>
2025-11-24 10:41:09 +0900

    feat: 허브 생성 구현 #7

--- src/main/java/com/smartlogis/hubservice/hub/domain/repository/HubQueryRepository.java
+++ src/main/java/com/smartlogis/hubservice/hub/domain/repository/HubQueryRepository.java
@@ -2,12 +2,8 @@ package com.smartlogis.hubservice.hub.domain.repository;
 
 import com.smartlogis.common.presentation.dto.PageRequest;
 import com.smartlogis.common.presentation.dto.PageResponse;
-import com.smartlogis.hubservice.hub.domain.Hub;
-import com.smartlogis.hubservice.hub.domain.HubId;
 import com.smartlogis.hubservice.hub.presentation.dto.HubDetailResponse;
 import com.smartlogis.hubservice.hub.presentation.dto.HubListResponse;
-
-import java.util.Optional;
 
 public interface HubQueryRepository {
 
