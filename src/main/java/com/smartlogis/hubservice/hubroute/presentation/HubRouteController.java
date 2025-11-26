e203d4d2968fb778c255f5a5a5c78aaeb0691d49
nineeko <gkxm1013@naver.com>
2025-11-26 09:13:16 +0900

    fix: gateway설정으로 인한 api 주소 수정 #17

--- src/main/java/com/smartlogis/hubservice/hubroute/presentation/HubRouteController.java
+++ src/main/java/com/smartlogis/hubservice/hubroute/presentation/HubRouteController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/v1/hub-routes")
+@RequestMapping("/v1/hubs/routes")
 @RequiredArgsConstructor
 public class HubRouteController {
 
--- src/main/java/com/smartlogis/hubservice/hubroute/presentation/HubRouteQueryController.java
+++ src/main/java/com/smartlogis/hubservice/hubroute/presentation/HubRouteQueryController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/v1/hub-routes")
+@RequestMapping("/v1/hubs/routes")
 @RequiredArgsConstructor
 public class HubRouteQueryController {
 
