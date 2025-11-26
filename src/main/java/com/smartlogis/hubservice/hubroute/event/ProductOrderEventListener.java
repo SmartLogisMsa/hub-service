2b7e5a71e41517337e5984e5dcd719a2b6098155
nineeko <gkxm1013@naver.com>
2025-11-26 18:07:50 +0900

    feat: 허브 이벤트 경로 수정 #21

--- src/main/java/com/smartlogis/hubservice/hubroute/event/ProductOrderEventListener.java
+++ src/main/java/com/smartlogis/hubservice/hubroute/event/ProductOrderEventListener.java
@@ -1,8 +1,8 @@
 package com.smartlogis.hubservice.hubroute.event;
 
-import com.smartlogis.hubservice.hubroute.event.message.ProductToHubRouteEvent;
 import com.smartlogis.hubservice.hubroute.application.service.RouteResolver;
 import com.smartlogis.hubservice.hubroute.event.message.DeliveryRouteEvent;
+import com.smartlogis.hubservice.hubroute.event.message.ProductToHubRouteEvent;
 import com.smartlogis.hubservice.hubroute.infrastructure.messaging.DeliveryRouteEventPublisher;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,24 +19,23 @@ public class ProductOrderEventListener {
 
     @RabbitListener(queues = "smartlogis.product.order.created.queue")
     public void onProductOrderCreated(ProductToHubRouteEvent event) {
-
-        log.info("[허브] 상품 이벤트 수신 productId={}, orderId={}",
-                event.getProductId(), event.getOrderId());
+        log.info("[허브] 상품 이벤트 수신 productId={}, orderId={}, event: {}",
+                event.productId(), event.orderId(), event);
 
         // routes 계산 (A→B→C 재귀 hop 포함)
         var routes = routeResolver.resolve(
-                event.getDepartureHubId(),
-                event.getDestinationHubId()
+                event.departureHubId(),
+                event.destinationHubId()
         );
 
         // 허브 → 배송 이벤트 발행
         DeliveryRouteEvent deliveryEvent = DeliveryRouteEvent.builder()
-                .orderId(event.getOrderId())
-                .productId(event.getProductId())
-                .departureHubId(event.getDepartureHubId())
-                .destinationHubId(event.getDestinationHubId())
-                .address(event.getAddress())
-                .receiptUserId(event.getReceiptUserId())
+                .orderId(event.orderId())
+                .productId(event.productId())
+                .departureHubId(event.departureHubId())
+                .destinationHubId(event.destinationHubId())
+                .address(event.address())
+                .receiptUserId(event.receiptUserId())
                 .routes(routes)
                 .build();
 
